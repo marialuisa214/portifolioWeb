3a0c6fd0fcc3b8954158c15aafb7e59104163332
Maria Luisa <maria.luisa.alves214@gmail.com>
2024-04-13 20:33:58 -0300

    :bug: fix: correção de imports

--- src/components/projects/index.tsx
+++ src/components/projects/index.tsx
@@ -1,4 +1,4 @@
-import { CarroselDiv, Container,ProjectTemplate } from "./styles";
+import { CarroselDiv, Container,ProjectTemplate } from './styles';
 
 const responsive = {
     superLargeDesktop: {
@@ -23,7 +23,7 @@ const responsive = {
 export function Projects() {
     const projects = [ 'projeto 1', 'projeto 2', 'projeto 3', 'projeto 4', 'projeto 5', 'projeto 6', 'projeto 7', 'projeto 8', 'projeto 9', 'projeto 10', 'projeto 11', 'projeto 12', 'projeto 13', 'projeto 14', 'projeto 15', 'projeto 16', 'projeto 17', 'projeto 18', 'projeto 19', 'projeto 20']
 
-    const colors = ['#ff0000', '#00ff00', '#0000ff', '#ffff00', '#00ffff', '#ff00ff', '#f0f0f0', '#0f0f0f', '#123456', '#654321', '#abcdef', '#fedcba', '#321cba', '#cba321', '#13579a', '#97531a', '#86420f', '#f1e2d3', '#b2a9c8', '#abc123'];
+    const colors = ['#ff0000', '#00ff00', '#0000ff', '#ffff00', '#00ffff', '#ff00ff', '#f0f0f0', '#0f0f0f', '#123456', '#654321', '#abcdef', '#fedcba', '#321cba', '#cba321', '#13579a', '#97531a', '#86420f', '#f1e2d3', '#b2a9c8', '#abc123']
 
     return (
         <Container>
@@ -33,7 +33,7 @@ export function Projects() {
                         <p>{project}</p>
                     </ProjectTemplate>
                 ))}
-            </CarroselDiv>;
+            </CarroselDiv>
 
         </Container>
     )
