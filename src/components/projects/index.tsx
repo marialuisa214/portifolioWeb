24a59027b72f1d02108399c1d5d331d2c00754c7
Maria Luisa <maria.luisa.alves214@gmail.com>
2024-04-13 00:39:31 -0300

    :sparkles: feat: carrossel integrado

--- src/components/projects/index.tsx
+++ src/components/projects/index.tsx
@@ -1,23 +1,35 @@
 import { CaretLeft, CaretRight } from "@phosphor-icons/react";
 import { ButtonIcon, Carousel, Container,ProjectTemplate } from "./styles";
+import { useState } from "react";
 
 export function Projects() {
     const projects = [ 'projeto 1', 'projeto 2', 'projeto 3', 'projeto 4', 'projeto 5', 'projeto 6', 'projeto 7', 'projeto 8', 'projeto 9', 'projeto 10', 'projeto 11', 'projeto 12', 'projeto 13', 'projeto 14', 'projeto 15', 'projeto 16', 'projeto 17', 'projeto 18', 'projeto 19', 'projeto 20']
+    
+    const [startIndex, setStartIndex] = useState(0);
+
+    const nextProject = () => {
+        setStartIndex(startIndex + 3 >= projects.length ? 0 : startIndex + 1);
+    };
+
+    const previousProject = () => {
+        setStartIndex(startIndex === 0 ? projects.length - 3 : startIndex - 1);
+    };
+
+
     return (
         <Container>
-            <ButtonIcon>
-                <CaretLeft size={35}/>
-
+            <ButtonIcon onClick={previousProject}>
+                <CaretLeft size={35} />
             </ButtonIcon>
             <Carousel>
-                    {projects.map((project, i) => (
-                        <ProjectTemplate>
-                    <p key={i}>{project}</p>
+            {projects.slice(startIndex, startIndex + 3).map((project, index) => (
+                    <ProjectTemplate key={index}>
+                        <p>{project}</p>
                     </ProjectTemplate>
-                    ))}
+                ))}
             </Carousel>
-            <ButtonIcon>
-                <CaretRight size={35}/>
+            <ButtonIcon onClick={nextProject}>
+                <CaretRight size={35} />
             </ButtonIcon>
         </Container>
     )
