f8c8f92a6acc393fcc65031294f8e46c0f12645d
Maria Luisa <maria.luisa.alves214@gmail.com>
2024-04-15 12:51:59 -0300

    :nail_care: feat: project componente

--- src/components/projects/index.tsx
+++ src/components/projects/index.tsx
@@ -1,4 +1,6 @@
-import { CarroselDiv, Container,ProjectTemplate } from './styles';
+import { CarroselDiv, Container,ProjectContainer, ProjectExtra, ProjectImg } 
+from './styles';
+import imgGato from '../../assets/image.png';
 
 const responsive = {
     superLargeDesktop: {
@@ -23,15 +25,28 @@ const responsive = {
 export function Projects() {
     const projects = [ 'projeto 1', 'projeto 2', 'projeto 3', 'projeto 4', 'projeto 5', 'projeto 6', 'projeto 7', 'projeto 8', 'projeto 9', 'projeto 10', 'projeto 11', 'projeto 12', 'projeto 13', 'projeto 14', 'projeto 15', 'projeto 16', 'projeto 17', 'projeto 18', 'projeto 19', 'projeto 20']
 
-    const colors = ['#ff0000', '#00ff00', '#0000ff', '#ffff00', '#00ffff', '#ff00ff', '#f0f0f0', '#0f0f0f', '#123456', '#654321', '#abcdef', '#fedcba', '#321cba', '#cba321', '#13579a', '#97531a', '#86420f', '#f1e2d3', '#b2a9c8', '#abc123']
 
     return (
         <Container>
             <CarroselDiv responsive={responsive}>
             {projects.map((project, index) => (
-                    <ProjectTemplate  key={index} cor={colors[(index) % colors.length]}>
-                        <p>{project}</p>
-                    </ProjectTemplate>
+                    <ProjectContainer  key={index}>
+                        <ProjectExtra>
+                          <p>Orientção a Objetos</p>
+                          <button>figma</button>
+                        </ProjectExtra>
+                        <ProjectImg>
+                          <img src={imgGato} alt="" />
+                        </ProjectImg>
+                        <div>
+                          <strong>{project}</strong>
+                          <p>Descrição do projeto</p>
+                          <p>tecnologias</p>
+                        </div>
+                        <div>
+                          <button>git</button>
+                        </div>
+                    </ProjectContainer>
                 ))}
             </CarroselDiv>
 
--- src/components/projects/styles.ts
+++ src/components/projects/styles.ts
@@ -4,24 +4,26 @@ import "react-multi-carousel/lib/styles.css";
 
 export const Container = styled.div`
   display: flex;
-  height: 500px;
+  height: 800px;
   width: 100%;
   justify-content: center;
   align-items: center;
   padding: 120px 30px;
   
 `
-
-export const ProjectTemplate = styled.div<{cor: string}>`
+export const ProjectContainer = styled.div`
   display: flex;
+  flex-direction: column;
+  gap: 30px;
   height: 100%;
+  padding: 25px 30px;
   min-width: calc(calc(100% - 46px) / 3);
   object-fit: cover;
   margin-left: 10px;
   margin-right: 10px;
-  
-  background-color: ${(props) => props.cor};
-
+  border-radius: 10px;
+  color: ${(props) => props.theme['white']};
+  background-color: ${(props) => props.theme['blue-500']};
   
   @media screen and (max-width: 900px){
     min-width: (calc(100% - 46px)/2);
@@ -30,6 +32,39 @@ export const ProjectTemplate = styled.div<{cor: string}>`
     min-width: calc(100% - 46px) ;
   }
 `
+export const ProjectExtra = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  font-family: "Tilt Neon", sans-serif;
+  font-size: 14px;
+  button{
+    border: 5px solid ${(props) => props.theme['white']};
+    cursor: pointer;
+  }
+
+`
+
+export const ProjectImg = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 50%;
+  border-radius: 10px;
+  background-color: ${(props) => props.theme['white']};
+  opacity: 0.8;
+  img{
+    padding: 10px;
+    border-radius: 10px;
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+
+  }
+
+`
+
 export const CarroselDiv = styled(Carousel)`
   height: 100%;
   width: 100%;
